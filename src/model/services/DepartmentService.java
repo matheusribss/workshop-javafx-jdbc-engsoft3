f070bc37ccae8773d7fd9d50b92e38dbad787639
Matheus Ribeiro <matheus.ribs.sjc@gmail.com>
2021-03-16 13:18:47 -0300

    Adicionando acesso ao banco de dados

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,18 +1,27 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	//Injetando a depedencia usando o Padrao de projetos Factory
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll(){
-		//MOCK = RETORNA DADOS DE MENTIRINHA
+		
+		return dao.findAll();
+		
+		
+		/*
+		//MOCKANDO DADOS = RETORNA DADOS DE MENTIRINHA
 		List<Department> list = new ArrayList<>();
 		list.add(new Department(1,"Livros"));
 		list.add(new Department(2,"Computadores"));
 		list.add(new Department(3,"Eletronicos"));
-		return list;
+		return list;*/
 	}
 }
