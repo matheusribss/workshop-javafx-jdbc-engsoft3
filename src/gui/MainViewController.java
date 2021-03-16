e71c327d66a57becc8af904c71aaa7a03a143c13
Matheus Ribeiro <matheus.ribs.sjc@gmail.com>
2021-03-16 12:22:58 -0300

    Inicializando o action como um parametro

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,14 +36,17 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	
@@ -50,8 +54,11 @@ public class MainViewController implements Initializable {
 	public void initialize(URL uri, ResourceBundle rb) {
 
 	}
-
-	private synchronized void loadView(String absoluteName) {
+	//Função do tipo <T> Função parametrizada com um tipo Qualquer
+	//Generic e expressoes Lambda
+	//para ter uma unica versao da função , eu fiz a parametrização com o Consumer <t> , vai carregar as janelas com ações ou sem , sem precisar criar mais funçoes
+	//mesmo efeito que tinha antes com o loadView2 , mas agora com uma função
+	private synchronized <T> void  loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			
 		
@@ -66,12 +73,15 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
+			T controller= loader.getController();
+			initializingAction.accept(controller);
+			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 	
-	private synchronized void loadView2(String absoluteName) {
+	/*private synchronized void loadView2(String absoluteName) {
 		try {
 			
 		
@@ -93,10 +103,10 @@ public class MainViewController implements Initializable {
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
+		}*/
 	}
 
-}
+
 
 
 
